db1d5bc5436cff98990e5ed33e7c253cf17430e0
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-08-02 15:36:17 -0300

    :sparkles: feat: realizando otimizações utilizando useCallback

--- components/ProductItem.tsx
+++ components/ProductItem.tsx
@@ -6,17 +6,21 @@ interface ProductItemProps {
     price: number;
     title: string;
   };
+  onAddToWishList: (id: number) => void;
 }
 
-function ProductItemComponent({ product }: ProductItemProps) {
+function ProductItemComponent({ product, onAddToWishList }: ProductItemProps) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
+      <button onClick={() => onAddToWishList(product.id)}>Add to Wishlist</button>
     </div>
   );
 }
 
-export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
-  return Object.is(prevProps.product, nextProps.product);
-});
- 
+export const ProductItem = memo(
+  ProductItemComponent,
+  (prevProps, nextProps) => {
+    return Object.is(prevProps.product, nextProps.product);
+  }
+);
--- components/searchResults.tsx
+++ components/searchResults.tsx
@@ -7,10 +7,14 @@ interface SearchResultsProps {
     price: number;
     title: string;
   }>;
+  onAddToWishList: (id: number) => void;
 }
 
-export function SearchResults({ results }: SearchResultsProps) {
-  const totalPrice = useMemo(() => {
+export function SearchResults({
+  results,
+  onAddToWishList,
+}: SearchResultsProps) {
+  const totalPrice = useMemo(() => {  
     return results.reduce((acc, curr) => {
       return acc + curr.price;
     }, 0);
@@ -21,7 +25,13 @@ export function SearchResults({ results }: SearchResultsProps) {
       <h2>{totalPrice}</h2>
 
       {results.map((product) => {
-        return <ProductItem product={product} />;
+        return (
+          <ProductItem
+            key={product.id}
+            product={product}
+            onAddToWishList={onAddToWishList}
+          />
+        );
       })}
     </div>
   );
