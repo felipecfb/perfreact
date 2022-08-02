76bffa473f73b2c098a61ad12316c8895546a221
Felipe Bastos | DT3 <felipe.bastos@dt3.com.br>
2022-08-02 14:43:59 -0300

    :heavy_plus_sign: adicionando json-server e configurando

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,39 @@
-const Home: NextPage = () => {
+import { FormEvent, useState } from "react";
+import { SearchResults } from "../components/searchResults";
+
+function Home() {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+
+  async function handleSearch(event: FormEvent) {
+    event.preventDefault();
+
+    if (!search.trim()) {
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`);
+    const data = await response.json();
+
+    setResults(data);
+  }
+
   return (
-    <h1>
-      Hello world
-    </h1>
-  )
+    <div>
+      <h1>Search</h1>
+
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button type="submit">Buscar</button>
+      </form>
+
+      <SearchResults results={results} />
+    </div>
+  );
 }
 
-export default Home
+export default Home;
